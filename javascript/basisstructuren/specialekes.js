849ad120b00fae3434e4222af67e24e62a060c08
Ann Van Eyken <ann@dbicts.lokaal>
2017-10-20 11:58:09 +0200

    specialekes.js (speciale uitkomsten van wisk bewerkingen) toegevoegd

---
+++ javascript/basisstructuren/specialekes.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var smurf;
+
+console.log(1+1);  // 2
+console.log("1"+1); // 11
+console.log(2*"10");  // 20
+console.log(2*"smurf");  // NaN
+console.log(5/0);   // Infinity
+console.log(smurf);   // undefined
+
+// we mogen (sleutelwoord) null toekennen aan een var
+// om aan te geven dat die var momenteel geen bruikbare waarde bevat
+smurf = null;
+console.log(smurf); // null
