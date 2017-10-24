344e595037112b8f0e7d63567a27beb483250ecd
User110 <rextans@gmail.com>
2017-10-24 08:47:59 +0200

    Verbeterde oefeningen

--- javascript/basisstructuren/eerste-oefening.js
+++ javascript/basisstructuren/eerste-oefening.js
@@ -11,6 +11,17 @@ var product = eersteGetal * tweedeGetal;
 
 console.log('Het product van %s en %s is', eersteGetal, tweedeGetal, product );
 
+/* 
+//betere oplosing
+'use string';
+var toetsenbord = require('readline-sync');
+
+var getal1 = toetsenbord.question('Tik je eerste getal in:');
+var getal2 = toetsenbord.question('Tik je tweede getal in:');
+
+console.log('Het product van %s en %s is', getal1, getal2, parseFloat(getal1)*parseFloat(getal2));
+*/
+
 var loon = toetsenbord.question('Tik je loon in: ');
 var loonsverhoging = toetsenbord.question('Tik je loonsverhoging in: ');
 
@@ -20,3 +31,15 @@ v = +loonsverhoging;
 var nieuweLoon = (l + v);
 
 console.log('Je nieuwe loons is %s', nieuweLoon, '=', loon, '+', loonsverhoging );
+
+/* 
+//betere oplosing 
+'use string';
+
+var toetsenbord = require('readline-sync');
+
+var loon = toetsenbord.question('Tik je loon in:');
+var loonsverhoging = toetsenbord.question('Tik je loons verhoging in:');
+
+console.log('Je loonsverhoging is', parseFloat(loon)+parseFloat(loonsverhoging));
+*/
