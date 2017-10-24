fb9675d2aee21de6f74c771b5672cf848aa0fcbb
annve <ann.vaneyken@betavzw.org>
2017-10-24 16:38:49 +0200

    for02.js onderaan aangepast

--- javascript/basisstructuren/for02.js
+++ javascript/basisstructuren/for02.js
@@ -7,3 +7,4 @@ for (var i = 0; i < tekst.length; i++) {
     if (tekst.charAt(i) == 'i') teller++;
 }
 console.log("De letter \"i\" komt " + teller + " maal voor");
+console.log('De letter "i" komt ' + teller + " maal voor");
