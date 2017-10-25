4ad56391ef77eec73c02971a59a96200a9285f79
annve <ann.vaneyken@betavzw.org>
2017-10-25 11:53:08 +0200

    functies01.js en functies02.js aangevuld met commentaar; oefFies01.js toegevoegd

--- javascript/basisstructuren/functies01.js
+++ javascript/basisstructuren/functies01.js
@@ -2,7 +2,7 @@
 
 var toetsenbord = require('readline-sync');
 var getal = toetsenbord.question('Tik een getal in: ');
-
+/*
 // wat doet onderstaand script?
 var resultaat = 1;
 for (var i = 2; i <= getal; i++) {
@@ -10,3 +10,30 @@ for (var i = 2; i <= getal; i++) {
 }
 
 console.log(resultaat);
+*/
+// om deze berekening leesbaarder te maken en 
+// toe te kunnen passen voor andere getallen, 
+// gebruiken we een functie:
+
+// functiedefinitie/-declaratie:
+function faculteit(x) {
+    console.log("functie wordt uitgevoerd")
+    var result = 1;
+    for (var j = 2; j <= x; j++) {
+        result *= j;
+    }
+    return result;
+    // return
+    // => beeindig functie en geef waarde die achter return staat terug als uitkomst
+    // (als een fie geen expliciete returnwaarde heeft, 
+    // geeft ze impliciet undefined terug)
+}
+
+
+// functie oproep:
+var resultaat = faculteit(getal);
+console.log(resultaat);
+console.log(faculteit(10));
+if( faculteit(5) < 20){
+    console.log("5! is kleiner dan 20");
+}
--- javascript/basisstructuren/functies02.js
+++ javascript/basisstructuren/functies02.js
@@ -11,6 +11,16 @@ function berekenTweedeMacht(getal) {
     return resultaat;
 }
 
+function berekenMacht(grondtal, exponent){
+    var resultaat = 1;
+    for(var i=1; i<= exponent; i++){
+        resultaat *= grondtal;
+    }
+    return resultaat;
+}
+
+/* de volgorde en plaats van de functiedefinities is onbelangrijk */
+
 // functies oproepen ("gebruiken")
 getal1 = toetsenbord.question("Geef eerste getal: ");
 macht = berekenTweedeMacht(getal1) ;
@@ -18,3 +28,5 @@ console.log("Het getal %d vermenigvuldigd met zichzelf is %d", getal1, macht);
 getal2 = toetsenbord.question("Geef tweede getal: ");
 macht = berekenTweedeMacht(getal2);
 console.log("Het getal %d vermenigvuldigd met zichzelf is %d", getal2, macht);
+macht = berekenMacht(getal1-1, getal2);   // als argument mag een berekening doorgegeven worden
+console.log("%d tot de %de macht is %d", getal1, getal2, macht); 
