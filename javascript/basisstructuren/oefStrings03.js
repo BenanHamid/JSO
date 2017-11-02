a0e83243d340781bb3eb103654ef75d4227afefd
annve <ann.vaneyken@betavzw.org>
2017-11-02 11:18:21 +0100

    oefStrings03.js uitgebreid

--- javascript/basisstructuren/oefStrings03.js
+++ javascript/basisstructuren/oefStrings03.js
@@ -5,6 +5,15 @@ var toetsenbord = require('readline-sync');
 var zin, i;
 var result = "";
 zin = toetsenbord.question("Geef een zin: ");
+
+// eerste poging:
+// result = zin.replace(",", ", ");  // fout (enkel eerste komma wordt vervangen)
+// met reguliere expressie (zie later):
+result = zin.replace(/,/g, ", ");
+console.log(result);
+
+// alternatief zonder reguliere expressie:
+result = "";
 for (i = 0; i < zin.length; i++) {
     result += zin.charAt(i);
     if (zin.charAt(i) == ",") {
@@ -12,5 +21,3 @@ for (i = 0; i < zin.length; i++) {
     }
 }
 console.log(result);
-
-
